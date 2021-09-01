a2e3cd79e1bd1f81b2c87680084ad7a217232c76
wanghong <896922176@qq.com>
2021-09-01 09:07:07 +0800

    删掉R.java文件

--- app/src/main/java/com/grace/gradleplugindemo/MainActivity.java
+++ app/src/main/java/com/grace/gradleplugindemo/MainActivity.java
@@ -3,14 +3,9 @@ package com.grace.gradleplugindemo;
 import androidx.appcompat.app.AppCompatActivity;
 //import androidx.appcompat.widget.AppCompatTextView;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import com.grace.gradleplugindemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
